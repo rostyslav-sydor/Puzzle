05c0c7d41b99bb29d34fb2b5f4a7a9517878d753
rostyslav-sydor <fat.bich228@gmail.com>
2021-02-05 20:59:10 +0200

    Added checkRows function

--- puzzle.py
+++ puzzle.py
@@ -1,12 +1,23 @@
 def checkColumns(board: list) -> bool:
     """
-    Returns true if there is no repeating numbers in the columns
+    Returns True if there is no repeating numbers in the columns.
     """
-    for i in range(9):
+    for i in range(len(board[0])):
         column = []
         for line in board:
             if line[i].isnumeric():
                 column.append(line[i])
         if len(column) != len(set(column)):
+            return False
+    return True
+
+
+def checkRows(board: list) -> bool:
+    """
+    Returns True if there is no repeating numbers in the rows.
+    """
+    for row in board:
+        numbersRow = row.replace('*', '').replace(' ', '')
+        if len(numbersRow) != len(set(numbersRow)):
             return False
     return True
